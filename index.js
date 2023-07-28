e8303d6675f5589a7ccfab4692d639ddc1a5b463
Fred Eugine Adomako <fredadomako2003@gmail.com>
2023-07-28 18:01:55 +0200

    DONE

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import cors from "cors";
 import express from 'express';
 import path from "path";
-import {getAll} from "./routes/handlers.js";
-import {getone} from "./routes/handlers.js";
-import {update} from "./routes/handlers.js";
-import {Add} from "./routes/handlers.js";
-import {Delete} from "./routes/handlers.js";
+import {getAll,getone,update,Add,Delete} from "./routes/handlers.js";
+import {fileURLToPath} from "url";
+
 var app = express();
 var port = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(cors());
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get('/api/items',getAll)
 app.get('/api/items/:id',getone)
@@ -27,5 +26,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, function () {
-    console.log('Backend server started');
+    console.log('Backend server started at port ' + port );
 });
